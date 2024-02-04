79102d82b24671e2a9683696f7c63fe40c7e8b65
Altyrion69 <altyrion@hotmail.fr>
2024-02-04 18:28:38 +0100

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,11 +12,13 @@ const Navbar = () => {
   return (
     <nav
       className={`${styles.paddingX} w-full flex
-       items-center py-5 fixed top-0 z-20
+       items-center py-5 fixed top-0 z-20 
         bg-primary`}
     >
-      <div className="w-full flex justify-between
-       items-center max-w-7xl mx-auto">
+      <div
+        className="w-full flex justify-between
+       items-center max-w-7xl mx-auto"
+      >
         <Link
           to="/"
           className="flex items-center gap-2"
@@ -25,11 +27,18 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9
-           h-9 object-contain" />
-          <p className="text-white text-[18px] 
-          font-bold cursor-pointer">Paul <span 
-          className="sm:block hidden">| Portofolio</span></p>
+          <img
+            src={logo}
+            alt="logo"
+            className="w-9
+           h-9 object-contain"
+          />
+          <p
+            className="text-white text-[18px] 
+          font-bold cursor-pointer"
+          >
+            Paul <span className="sm:block hidden">| Portofolio</span>
+          </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
@@ -59,7 +68,21 @@ const Navbar = () => {
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute
           top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          ></div>
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => setActiveLink(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
